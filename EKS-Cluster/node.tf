9cd6466c53092c43c8385e44f72afd9d415e993e
SaurabhRokde <srokde15@gmail.com>
2023-12-22 08:51:56 +0530

    created EKS-cluster using terraform

---
+++ EKS-Cluster/node.tf
@@ -0,0 +1,41 @@
+#Node
+resource "aws_instance" "kubectl-server" {
+  ami                         = "ami-008fe2fc65df48dac"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public-1.id
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "kubectl"
+  }
+
+}
+
+resource "aws_eks_node_group" "node-group" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "my-pc-node-group"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  capacity_type   = "ON_DEMAND"
+  disk_size       = "20"
+  instance_types  = ["t2.medium"]
+
+  labels = tomap({ env = "dev" })
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+
+  ]
+}
